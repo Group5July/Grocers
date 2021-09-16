d9b536caa6195d3b85ad3ef1aa70e71c3ad0d541
Carlos Ovalle <carlos.ovalle21@utexas.edu>
2021-09-16 12:58:49 -0500

    updated router

--- Frontend/capstone/src/app/app-routing.module.ts
+++ Frontend/capstone/src/app/app-routing.module.ts
@@ -9,6 +9,8 @@ const routes: Routes = [
   {path:"signin", component:EmployeeSigninComponent},
   //{path:"employee-dashboard/:user", component:EmployeeDashboardComponent},
   {path:"employee-dashboard", component:EmployeeDashboardComponent}, //for testing
+  {path:"admin-dashboard", component:AdminDashboardComponent}, //for testing
+  {path:"admin-signin", component:AdminSigninComponent},
   //{path:"employeelogout",component:DashboardComponent}, 
   //{path:"employeeDashboard",component:EmployeeDashboardComponent},
   {path:"", redirectTo:"home", pathMatch:"prefix"} //empty val passed then goto login
