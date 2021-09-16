3837f060ab49adaab90d2065a8d6f99750709bf1
Carlos Ovalle <carlos.ovalle21@utexas.edu>
2021-09-16 18:52:20 -0500

    working home page

--- Frontend/capstone/src/app/main/main.component.ts
+++ Frontend/capstone/src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute,Router} from '@angular/router'
 
 @Component({
   selector: 'app-main',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(public activateRoute:ActivatedRoute,public router:Router) { } 
 
   ngOnInit(): void {
+
+  }
+
+  go_toadmin(){
+  	this.router.navigate(["admin-signin"])
+  }
+
+  go_touser(){
+  	this.router.navigate(["user-signin"])
+  }
+
+  go_toemployee(){
+  	this.router.navigate(["signin"])
   }
 
 }
