217b21425f43a3abc10a96f5aade5ef4571ba5ad
Nirja Godhani <ng552@scarletmail.rutgers.edu>
2021-09-16 17:48:07 -0400

    changes in the user panel

--- Frontend/capstone/src/app/user-panel/user-panel.component.ts
+++ Frontend/capstone/src/app/user-panel/user-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute,Router} from '@angular/router'
 
 @Component({
   selector: 'app-user-panel',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserPanelComponent implements OnInit {
 
-  constructor() { }
+  viewCart_mini_comp = false;
+  editProfile_mini_comp = false;
+  fund_mini_comp = false;
+  orderStatus_mini_comp = false;
+  checkOut_mini_comp = false;
+  
+  constructor(public activateRoute:ActivatedRoute,public router:Router) { } 
 
   ngOnInit(): void {
+  }
+
+  a_ViewCart(){
+    this.set_minicomps_false();
+    this.viewCart_mini_comp = true;
+  }
+
+  a_EditProfile(){
+    this.set_minicomps_false();
+    this.editProfile_mini_comp = true;
+  }
+
+  a_Fund(){
+    this.set_minicomps_false();
+    this.fund_mini_comp = true;
+  }
+  
+  a_OrderStatus(){
+    this.set_minicomps_false();
+    this.orderStatus_mini_comp = true;
+  }
+  a_CheckOut(){
+    this.set_minicomps_false();
+    this.checkOut_mini_comp = true;
+  }
+
+  set_minicomps_false(){
+    this.viewCart_mini_comp = false;
+    this.editProfile_mini_comp = false;
+    this.fund_mini_comp = false;
+    this.orderStatus_mini_comp = false;
+    this.checkOut_mini_comp = false;
   }
 
 }
