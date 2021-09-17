16041d8b77976b22e198fbc43f0b48fa6a53ac70
Nirja Godhani <ng552@scarletmail.rutgers.edu>
2021-09-17 05:33:33 -0400

    commit

--- Backend/model/RaiseTicket.model.js
+++ Backend/model/RaiseTicket.model.js
@@ -2,13 +2,13 @@ let mongoose = require("mongoose");
 
 mongoose.pluralize(null);
 
-let userSchema = mongoose.Schema({
+let raiseTicketSchema = mongoose.Schema({
     userId:{type:String, unique:true},
     resolved:{type:Boolean},
     msg:{type:String}
   
 });
 
-let raiseTicketModel = mongoose.model("user", userSchema);
+let raiseTicketModel = mongoose.model("user", raiseTicketSchema);
 
 module.exports = raiseTicketModel;
