7849b0b5d2714a27a13e8368469a2e059f3cec40
Clara Martinez Vargas <cmmartinezvargas95@gmail.com>
2021-09-17 01:54:00 -0500

    no

--- Backend/model/Employee.model.js
+++ Backend/model/Employee.model.js
@@ -4,7 +4,10 @@ mongoose.pluralize(null);
 
 let employeeSchema = mongoose.Schema({
     employeeID:{type:String,unique:true},
-    employeePassword:{type:String}
+    employeePassword:{type:String},
+    emailID:{type:String},
+    firstName:{type:String},
+    lastName:{type:String},
 });
 
 let employeeModel = mongoose.model("Employee",employeeSchema);
